04b5eea058a35dc6c4ee0893f10db6a50cd24993
UkiHito <89295429467@mail.ru>
2017-10-26 14:11:52 +0300

    Update

--- js/custom.js
+++ js/custom.js
@@ -10,10 +10,26 @@ $(document).ready(function(){
   });
 });
 $(document).ready(function() {
+
     $('.progress .progress-bar').progressbar({
         transition_delay: 10
     });
     $('#myTabs li a').click(function(){
 
     });
+
+    $('.progress-bar').each(function() {
+      var bar_value = $(this).attr('aria-valuenow') + '%';
+      $(this).animate({ width: bar_value }, { duration: 2000 });
+    });
+
+    // Shine text
+    var scrollSpeed = 70;
+    var current = 0;
+    function bgscroll(){
+        current -= 10;
+        // move the background with backgrond-position css properties
+        $('body').css("backgroundPosition", 1 ? current+"px 0" : "0 " + current+"px");
+    }
+    setInterval(bgscroll, scrollSpeed);
 });
